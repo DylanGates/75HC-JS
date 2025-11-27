72ecea5247dd64f238cf0f3111bf7efb10098a93
DylanGates <41268880+DylanGates@users.noreply.github.com>
2025-11-27 23:05:22 +0000

    added reset button

--- ClickCounter/src/App.tsx
+++ ClickCounter/src/App.tsx
@@ -30,6 +30,12 @@ function App() {
       <div className="mt-4 bg-gray-200 p-4 rounded opacity-80">
         <p className="text-gray-800">Count: {count}</p>
       </div>
+      <button
+        className="bg-red-500 mt-4 text-white px-4 py-2 rounded hover:bg-red-600"
+        onClick={() => setCount(0)}
+      >
+        reset
+      </button>
     </>
   );
 }
