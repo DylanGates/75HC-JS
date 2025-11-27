065898f2148b329e5da6b6d98a5bd83c666fc2a5
DylanGates <41268880+DylanGates@users.noreply.github.com>
2025-11-27 23:00:11 +0000

    added decrement

--- ClickCounter/src/App.tsx
+++ ClickCounter/src/App.tsx
@@ -1,18 +1,31 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  // const [decrement, setDecrement] = useState(0)
 
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="flex flex-row gap-4">
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count
+        </button>
+        <button
+          className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600"
+          onClick={() => setCount((count) => count - 1)}
+        >
+          decrement
         </button>
       </div>
+      <div className="mt-4 bg-gray-200 p-4 rounded opacity-80">
+        <p className="text-gray-800">Count: {count}</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
