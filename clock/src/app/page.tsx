472539a6b7e54cf9c98c9bf023e0211f45f5ce03
DylanGates <41268880+DylanGates@users.noreply.github.com>
2025-12-09 19:16:25 +0000

    minor update

--- clock/src/app/page.tsx
+++ clock/src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useState, useEffect } from "react";
 
@@ -9,7 +9,7 @@ export default function Home() {
     const timer = setInterval(() => {
       setTime(new Date());
     }, 1000);
-  return () => clearInterval(timer);
+    return () => clearInterval(timer);
   }, []);
 
   let h = time.getHours().toString().padStart(2, "0");
@@ -20,22 +20,23 @@ export default function Home() {
   sessionStorage.setItem("minute", m);
   sessionStorage.setItem("second", s);
 
-  sessionStorage.setItem("ampm", h >= '12' ? 'PM' : 'AM');
+  sessionStorage.setItem("ampm", h >= "12" ? "PM" : "AM");
 
-  if(parseInt(h) == 0) {
-    h = '12';
+  if (parseInt(h) == 0) {
+    h = "12";
   }
   if (parseInt(h) > 12) {
     h = (parseInt(h) - 12).toString().padStart(2, "0");
-
   }
+
+  h = parseInt(h).toString();
 
   return (
     <div>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
         <div className="bg-white p-10 rounded-lg shadow-lg">
           <h1 className="text-6xl font-bold mb-4">
-            {h}:{m}:{s} {time.getHours() >= 12 ? 'PM' : 'AM'}
+            {h}:{m}:{s} {time.getHours() >= 12 ? "PM" : "AM"}
           </h1>
         </div>
       </div>
