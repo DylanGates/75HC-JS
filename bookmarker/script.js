a119cdabf19d9ea565a5d08c6f266fba49e47704
DylanGates <solomonafoh@gmail.com>
2026-03-09 20:21:24 +0000

    add html skeleton

---
+++ bookmarker/script.js
@@ -0,0 +1,19 @@
+// script.js - initial scaffold; logic will be added in subsequent commits
+document.addEventListener('DOMContentLoaded', () => {
+  const addBtn = document.getElementById('addBtn');
+  const exportBtn = document.getElementById('exportBtn');
+  const importFile = document.getElementById('importFile');
+
+  addBtn.addEventListener('click', () => {
+    // placeholder: add logic will be implemented in next commit
+    alert('Add logic not implemented yet.');
+  });
+
+  exportBtn.addEventListener('click', () => {
+    alert('Export not implemented yet.');
+  });
+
+  importFile.addEventListener('change', () => {
+    alert('Import not implemented yet.');
+  });
+});
